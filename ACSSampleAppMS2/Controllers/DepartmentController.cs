fd6f4751ac2da4eb52488f0a086c134f48edd619
Abhirup Guha <abhirup.guha94@gmail.com>
2017-01-23 11:50:22 +0000

    ACS Sample APP

---
+++ ACSSampleAppMS2/Controllers/DepartmentController.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ACSSampleAppMS2.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public IEnumerable<string> GetDepartments()
+        {
+            return new List<string> { "Dept1", "Dept2", "Dept33" };
+        }
+    }
+}
